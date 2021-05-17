87a5349746cb047277c0bef535859cff500363cd
Julieta <julietadimarco@gmail.com>
2021-05-17 20:41:51 -0300

    Creado cMoto, cVehiculo terminado excepto to_string

--- Trabajo Practico 3 - 2021/cVehiculo.cpp
+++ Trabajo Practico 3 - 2021/cVehiculo.cpp
@@ -2,4 +2,27 @@
 
 cVehiculo::cVehiculo(string patente_c, string chasis_c, string poliza_c, eColores color_c, int cant_pasajeros_c):patente(patente_c), chasis(chasis_c), poliza(poliza_c)
 {
+	color = color_c;
+	cant_pasajeros = cant_pasajeros_c;
+	Ultimo_Mantenimiento = new cFecha();
+}
+
+cVehiculo::~cVehiculo()
+{
+	if (Ultimo_Mantenimiento != NULL)
+		delete Ultimo_Mantenimiento;
+}
+
+void cVehiculo::ActualizarMantenimiento() {
+	Ultimo_Mantenimiento->SetHoy();
+}
+
+string cVehiculo::to_string()
+{
+	return string();
+}
+
+void cVehiculo::Imprimir()
+{
+	cout << to_string();
 }
--- Trabajo Practico 3 - 2021/cVehiculo.h
+++ Trabajo Practico 3 - 2021/cVehiculo.h
@@ -3,26 +3,26 @@
 #include <string>
 #include "Definiciones.h"
 #include "cFecha.h"
-
-
 using namespace std;
+
 class cVehiculo
 {
 protected:	
 	const string chasis;
 	const string poliza;
-	//fecha ultimo mantenimiento
+	cFecha* Ultimo_Mantenimiento;
 	
 public:
 	const string patente;
 	int cant_pasajeros;
 	eColores color;
 #pragma region Constructores y Destructores
+//Constructor. Inicializa la fecha de último mantenimiento como la fecha actual.
 	cVehiculo(string patente_c, string chasis_c, string poliza_c, eColores color_c, int cant_pasajeros_c);
-	
+	~cVehiculo();
 #pragma endregion
 #pragma region Metodos
-
+	void ActualizarMantenimiento();
 #pragma endregion
 #pragma region Métodos virtuales
 	//calcula el precio del alquiler del vehículo (tarifa base + precio por día) sin tener en cuenta los adicionales
@@ -30,7 +30,12 @@ public:
 	/*calcula el precio del alquiler del vehículo (tarifa base + precio por día) sin tener en cuenta los adicionales
 	Recibe la fecha de inicio */
 	virtual float CalcularTarifa(cFecha& fecha_inicio, cFecha& fecha_fin) = 0;
+	//Imprime los pasos de mantenimiento para el vehículo
+	virtual void PasosMantenimiento() = 0;
 #pragma endregion
-
+#pragma region To_string e Imprimir
+	string to_string();
+	void Imprimir();
+#pragma endregion
 };
 
