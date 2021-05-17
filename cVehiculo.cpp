6d6662ea186a7de6f4b89a46361fa45214c537be
JulietaDiMarco <39888602+JulietaDiMarco@users.noreply.github.com>
2021-05-17 19:49:12 -0300

    Delete cVehiculo.cpp

--- cVehiculo.cpp
+++
@@ -1,5 +0,0 @@
-#include "cVehiculo.h"
-
-cVehiculo::cVehiculo(string patente_c, string chasis_c, string poliza_c, eColores color_c, int cant_pasajeros_c):patente(patente_c), chasis(chasis_c), poliza(poliza_c)
-{
-}
